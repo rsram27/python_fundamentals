afa798ad53a99b677b01b6a2c63f7458962920a7
rsram27 <rsram27@hotmail.com>
2019-11-13 19:30:14 -0300

    exercicio de revisao

---
+++ revisao.py
@@ -0,0 +1,44 @@
+#strings = 'palavra' ou "palavra" ou 'numero' ou "numero"
+#inteiro = 1- pi 15 ou -10 ou -15
+#float = 1.50 ou 10.18 ou -1.5 ou -10.18 ou 10.00 ou -10.00
+#booleano = True e False
+
+
+# s = False
+
+# if s is True:
+#     print('verdade')
+# else:
+#     print('mentira')
+
+
+# strings
+
+#rua = 'vergueiro'
+endereco = 'Rua Vergueiro 1057'
+
+# print(rua.upper())
+# print(rua.lower())
+
+
+# print(endereco.split('r'))
+
+# listas
+# lista_1 = [1, 2, 3, 'abacaxi', 'casa', 'carro', 1.4, True]
+
+# print(lista_1[1]) # indice da lista
+# #(lista_1.append('casa2')) # adicionar item na lista
+# lista_1.append('abacaxi')
+# #(lista_1.pop(2)) # remover pelo indice na lista
+
+# #lista_1.remove('abacaxi') # remover pelo nome do item da lista
+# lista_1.sort # ordena a lista usar sem o parentesis
+
+# print(lista_1)
+
+# tuplas
+
+# tupla = (1,2,3,4) # imutavel
+tupla = 1,2,3 # mutavel
+print(tupla.count(1)) # conta itens dentro da tupla
+print(tupla.index(1)) # retorna o iten da tupla
