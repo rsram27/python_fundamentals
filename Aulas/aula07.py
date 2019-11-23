a2606bc2125e73cc8a8040b22090aa52538616f3
rsram27 <rsram27@hotmail.com>
2019-11-22 21:19:13 -0300

    correcao aula 07

--- Aulas/aula07.py
+++ Aulas/aula07.py
@@ -1,82 +1,184 @@
-# # poo (herança e polimorfismo)
+# -*- coding: utf-8 -*- 
 
-# class Pai():
-#     '''Classe Pai'''
+# class Servidor:
+#     memoria = None
+#     disco = None
+#     cpu = None
+
+
+# dns = Servidor()
+
+# dns.memoria = 2048
+# dns.disco = 50
+# dns.cpu = 2
+
+# print(f'O servido tem as seguintes \
+#        \nconfigurações: \nCPU: {dns.cpu},\
+#        \nMemoria: {dns.memoria}, \
+#        \nDisco: {dns.disco}')
+
+# class PrimeiraClasse:
 
 #     def __init__(self):
-#         self.sobrenome = 'Pereira'
-
-#     def get_sobrenome(self):
-#         print(self.sobrenome)
-
-# # class Filho(Pai):
-
-# #     def __init__(self):
-# #         super().__init__() # HERDA O ATRIBUTO
-
-# class Animal():
-
-#     def __init__(self, raca, idade):
-#         self.raca = raca
-#         self.idade = idade
-#         self.olhos = True
-#         self.orgaos = True
-
-#     def Nascer(self):
-#         print('Nasceu...')
-
-#     def AlimentarSe(self):
-#         print('Se alimentando...')
-
-#     def Morrer(self):
-#         print('Morreu...')
+#         print('Acessando o método construtor')
     
-#     def get_raca(self):
-#         print(self.raca)
+#     def metodo(self):
+#         print('Acessando método')
+
+
+# print(PrimeiraClasse())
+
+# class Passaro():
+
+#     def __init__(self):
+#         self.asa = 2
+#         self.bico = 1
+#         self.penas = True
+#         self.patas = 2
+#         self.rabo = True
     
-#     def get_idade(self):
-#         print(self.idade)
-
-
-# class Mamifero(Animal):
-
-#     def __init__(self, raca, idade):
-#         super().__init__(raca, idade)
-
-#         self.pele = True
-#         self.mamilos = True
-
-# cachorro = Mamifero('Pastor', 7)
-# cachorro.get_raca()
-
-
-# class Humano(Mamifero):
-
-#     def __init__(self, nome, idade, pensa=True):
-#         super().__init__()
-#         self.nome = nome
-#         self.idade = idade
-#         self.pensa = pensa
-#         self.bracos = 2
-#         self.pernas = 2
-#         self.cabeca = 1
-#         self.tronco = 1
+#     def voar(self):
+#         if self.asa < 2:
+#             self.t = 1
+#             print('Passaro deficiente')
+#         else:
+#             print('Voando...')
+#             self.t = 0
     
-# humano1 = Humano('João', 21)
+#     def pousar(self):
+#         if self.t == 1:
+#             pass
+#         else:
+#             print('Pousando...')
 
-# print(humano1.nome, humano1.idade, humano1.pensa)
-
-class Pai():
+#     def dormir(self):
+#         print('Dormindo...')
     
-    def trabalhar(self):
-        print('Trabalhando de engenheiro...')
+#     def acordar(self):
+#         print('Acordadndo...')
 
-class Filho():
-    def trabalhar(self):
-        print('Trabalhando de programador...')
 
-joao = Pai()
-joao.trabalhar()
+# sabia = Passaro()
+# sabia.patas = 1
+# sabia.asa = 1
+# sabia.rabo = None
 
-joaquim = Filho()
-joaquim.trabalhar()
+# sabia.voar()
+# sabia.pousar()
+
+
+import random
+
+print(random.randrange(1, 100))
+
+
+
+
+
+# crie uma classe que represente um automovel
+
+# com os atributos:
+
+# ano de fabricação;
+# marca;
+# preco;
+
+# e os métodos:
+
+# get_ano;
+# get_marca;
+# get_preco;
+
+
+class Automovel():
+    '''Classe que representa um automovel'''
+    def __init__(self,ano, marca, preco):
+        self.ano = ano
+        self.marca = marca
+        self.preco = preco
+    
+    def get_ano(self):
+        print(self.ano)
+    
+    def get_marca(self):
+        print(self.marca)
+
+    def get_preco(self):
+        print(self.preco)
+        
+# crie uma classe Moto que terá:
+# o atributo tipo;
+# e herdará os atributos/métodos da classe automovel
+
+# e os métodos:
+
+
+# obs. lembresse que a moto só pode ligar se estiver desligada 
+# e desligar quando ligada
+# acelerar e frear também só ligada
+# ligar, desligar, acelerar, frear
+
+ligada = False
+
+class Moto(Automovel):
+    '''Classe que representa uma Moto'''
+    def __init__(self, ano, marca, preco, tipo='Moto'):
+        super().__init__(ano, marca, preco)
+        self.tipo = tipo
+
+    def ligar(self):
+        global ligada
+        try:
+            if ligada == False:
+                print('Ligando...')
+                print('Ligada...')
+                ligada = True
+            else:
+                raise TypeError('Moto ligada')
+        except TypeError as motoLigada:
+            print(motoLigada)
+
+
+    def desligar(self):
+        global ligada
+        try:
+            if ligada == True:
+                print('Desligando...')
+                print('Desligada...')
+                ligada = False
+            else:
+                raise TypeError('Moto desligada...')
+        except TypeError as m:
+            print(m)
+    
+    def acelerar(self):
+        global ligada
+        try:
+            if ligada == True:
+                print('Acelerando...')
+            else:
+                raise TypeError('Moto Desligada')
+        except TypeError as motoDesligada:
+            print(motoDesligada)
+        
+    def frear(self):
+        global ligada
+        try:
+            if ligada == True:
+                print('Freando...')
+            else:
+                raise TypeError('Moto Desligada')
+        except TypeError as motoDesligada:
+            print(motoDesligada)
+        
+
+
+
+cb125 = Moto(2010, 'Honda', 15.999)
+cb125.ligar()
+cb125.acelerar()
+cb125.frear()
+cb125.desligar()
+cb125.get_ano()
+cb125.get_marca()
+cb125.get_preco()
